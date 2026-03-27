179a9342f366d9f5b22fe794832a1324c2db2644
Zeljko Svedic <zeljko.svedic@testdome.com>
2026-03-27 14:59:36 +0100

    After iteration 7, all tests pass.

--- src-cli/tests/test3-tree.sh
+++ src-cli/tests/test3-tree.sh
@@ -6,14 +6,16 @@ source "$(dirname "$0")/base-testing.sh" "$0"
 
 ./stirr-tree.py tests/test-dir > "$LOG" 2>&1
 
-EXPECTED=$(cat <<'EOF'
-FILE TREE:
-. 0.29KB
-  #AI.#Test 0.00KB
-  test-hashtags.txt 0.29KB #human (4#foo 3#foobar 2#bar 1#human 1#foo-bar)
-TAG TOTALS:
+EXPECTED=$(cat <<'TXT'
+== FILE TREE as NAME SIZE (LOC LTOK) #FirstTag (Top 3 tags) ===
+test-dir/ 0.29 KB (12 LOC 59 LTOK)
+  #AI.#Test 0.00 KB (0 LOC 0 LTOK)
+  test-hashtags.txt 0.29 KB (12 LOC 59 LTOK) #human (4#foo 3#foobar 2#bar)
+== TAG TOTALS ===
 4#foo 3#foobar 2#bar 1#foo-bar 1#human
-EOF
+TXT
 )
 
+ACTUAL="$(cat "$LOG")"
+[[ "$ACTUAL" == "$EXPECTED" ]]
 echo Pass
